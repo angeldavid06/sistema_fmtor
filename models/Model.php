845635be0db752e69483fa27d4862358423d8a5c
angeldavid06 <mendozaangel.3243@gmail.com>
2021-09-29 10:04:49 -0500

    2021-09-29 09:51 am Estructura general del Proyecto Integrador

--- models/Model.php
+++ models/Model.php
@@ -1,7 +1,37 @@
 <?php
 
+    require_once "config/conexion.php";
+
     class Model {
-        
+        public $db;
+
+        public function __construct(){
+            $this->db= conexion::conectar();
+        }
+
+        public function mostrar ($tabla){
+            $sql="SELECT * FROM $tabla ";
+            $mostrar = $this->db->query($sql);
+            return $mostrar;
+        }
+
+        public function insertar ($tabla,$columnas,$valores){
+            $sql="INSERT INTO $tabla($columnas) VALUES ($valores)";
+            $insert=$this->db->query($sql);
+            return $insert;
+        }
+
+        public function actualizar ($tabla,$valores,$condicion) {
+            $sql="UPDATE $tabla set $valores WHERE $condicion";
+            $actu=$this->db->query($sql);
+            return $actu;
+        }
+
+        public function eliminar ($tabla,$condicion) {
+            $sql="DELETE FROM $tabla WHERE $condicion";
+            $eliminar=$this->db->query($sql);
+            return $eliminar;
+        }
     }
 
 ?>
