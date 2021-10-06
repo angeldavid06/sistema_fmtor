353f1b61e6720dfd8456673cf6d787efa7e3569d
angeldavid06 <mendozaangel.3243@gmail.com>
2021-10-06 10:19:12 -0500

    2021-10-06 08:49 am IImplementación de menus en todas las secciones

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
-    // require_once 'autoload.php';
 
-    // if (isset($_GET['controller']) && class_exists($_GET['controller'])) {
-    //     $controlador = $_GET['controller'];
-    //     $obj = new $controlador();
-    //     if (isset($_GET['action']) && method_exists($controlador,$_GET['action'])) {
-    //         $metodo = $_GET['action'];
-    //         $obj->$metodo();
-    //     } else {
-    //         echo 'el método no existe';
-    //     }
-    // } else {
-    //     require_once 'views/login.php';
-    // }
-    
     require_once __DIR__.'\libs/app.php';
     $app = new App();
 
