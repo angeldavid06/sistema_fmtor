98df88d2cd9723fecce8ad8f32a5db6434e21d7d
PerlaRamosL <17106775@tecnmtlahuac.onmicrosoft.com>
2021-11-23 14:30:36 -0600

    2021-11-23 14:30 Reporte Control Produccion

---
+++ public/pdf/produccion/modules_pdf/v_ranurado.php
@@ -0,0 +1,15 @@
+<table>
+    <thead>
+        <tr>
+            <th colspan="5">RANURADO</th>    
+       </tr>
+       <tr>
+           <th> </th>
+               
+       </tr>        
+    </thead>
+    <tbody>
+    </tbody>
+    <tfoot>
+    </tfood>
+</table>
