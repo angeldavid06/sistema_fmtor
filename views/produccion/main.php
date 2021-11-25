52cc14b135fb0e2241aedeb500e9b32079d60013
angeldavid06 <mendozaangel.3243@gmail.com>
2021-11-25 17:08:59 -0600

    2021-11-25 04:42 pm Formatos Producción

--- views/produccion/main.php
+++ views/produccion/main.php
@@ -13,31 +13,50 @@
         <div class="contenido hidde_menu" id="contenido">
             <?php require_once 'public/modules/depto.php'; ?>
             <div class="informacion">
-                <h2>Producción</h2>
-                <main>
-                    <nav class="d-grid g-2">
-                        <a href="http://localhost/sistema_fmtor/produccion/main/ordenes"
+                <h2>Acciones rápidas</h2>
+                <div class="tarjeta-transparente">
+                    <div class="d-flex flex-nowrap justify-left">
+                        <button class="btn btn-transparent btn-icon">
+                            <i class="material-icons">description</i>
+                            Ordenes de Producción
+                        </button>
+                        <button class="btn btn-transparent btn-icon">
+                            <i class="material-icons">add</i>
+                            Control de Producción
+                        </button>
+                        <button class="btn btn-transparent btn-icon">
+                            <i class="material-icons">description</i>
+                            Reporte Diario de Producción
+                        </button>
+                    </div>
+                </div>
+                <div class="tarjeta-transparente">
+                    <h2>Producción</h2>
+                    <main>
+                        <nav class="d-grid g-2">
+                            <a href="http://localhost/sistema_fmtor/produccion/main/ordenes"
+                                class="tarjeta d-flex align-content-center">
+                                <i class="material-icons icon">engineering</i>
+                                ORDENES DE PRODUCCIÓN
+                            </a>
+                            <a href="http://localhost/sistema_fmtor/produccion/main/control"
                             class="tarjeta d-flex align-content-center">
-                            <i class="material-icons icon">engineering</i>
-                            ORDENES DE PRODUCCIÓN
-                        </a>
-                        <a href="http://localhost/sistema_fmtor/produccion/main/control"
-                        class="tarjeta d-flex align-content-center">
-                            <i class="material-icons icon">supervisor_account</i>
-                            CONTROL DE PRODUCCIÓN
-                        </a>
-                        <a href="http://localhost/sistema_fmtor/produccion/main/maquinas"
-                        class="tarjeta d-flex align-content-center">
-                            <i class="material-icons icon">precision_manufacturing</i>
-                            MÁQUINAS DE PRODUCCIÓN
-                        </a>
-                        <a href="http://localhost/sistema_fmtor/produccion/main/estados"
-                        class="tarjeta d-flex align-content-center">
-                            <i class="material-icons icon">new_releases</i>
-                            ESTADO DE LAS ORDENES DE PRODUCCIÓN
-                        </a>
-                    </nav>
-                </main>
+                                <i class="material-icons icon">supervisor_account</i>
+                                CONTROL DE PRODUCCIÓN
+                            </a>
+                            <a href="http://localhost/sistema_fmtor/produccion/main/maquinas"
+                            class="tarjeta d-flex align-content-center">
+                                <i class="material-icons icon">precision_manufacturing</i>
+                                MÁQUINAS DE PRODUCCIÓN
+                            </a>
+                            <a href="http://localhost/sistema_fmtor/produccion/main/estados"
+                            class="tarjeta d-flex align-content-center">
+                                <i class="material-icons icon">new_releases</i>
+                                ESTADO DE LAS ORDENES DE PRODUCCIÓN
+                            </a>
+                        </nav>
+                    </main>
+                </div>
             </div>
         </div>
     </div>
