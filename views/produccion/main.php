566abc64d6138a3de44696ec5709e0744e14ce37
angeldavid06 <mendozaangel.3243@gmail.com>
2021-12-17 18:05:33 -0600

    2021-12-17 06:05 pm Nuevo menu principal

--- views/produccion/main.php
+++ views/produccion/main.php
@@ -12,7 +12,7 @@
         </a>
         <div class="contenido hidde_menu" id="contenido">
             <?php require_once 'public/modules/depto.php'; ?>
-            <div class="informacion">
+            <!-- <div class="informacion">
                 <h2>Acciones rápidas</h2>
                 <div class="tarjeta-transparente">
                     <div class="d-flex flex-nowrap justify-left">
@@ -56,6 +56,67 @@
                             </a>
                         </nav>
                     </main>
+                </div>
+            </div> -->
+            <div class="informacion">
+                <div class="tarjeta-transparente menu-principal">
+                    <h2>General</h2>
+                    <div class="d-grid g-3">
+                        <a class="btn btn-transparent" href="http://localhost/sistema_fmtor/sii/main/">
+                            <div class="d-grid g-1">
+                                <i class="material-icons-outlined icon">people</i>
+                                <span class="material-icons-outlined">people</span>
+                                <p>INFORMACIÓN PERSONAL</p>
+                            </div>
+                        </a>
+                        <a class="btn btn-transparent" href="http://localhost/sistema_fmtor/sii/main/">
+                            <div class="d-grid g-1">
+                                <i class="material-icons-outlined icon">people</i>
+                                <span class="material-icons-outlined icon">people</span>
+                                <p>CAJA DE AHORRO</p>
+                            </div>
+                        </a>
+                        <a class="btn btn-transparent" href="http://localhost/sistema_fmtor/sii/main/">
+                            <div class="d-grid g-1">
+                                <i class="material-icons-outlined icon">people</i>
+                                <span class="material-icons-outlined icon">people</span>
+                                <p>PRESTAMOS</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="tarjeta-transparente menu-principal">
+                    <h2>Producción</h2>
+                    <div class="d-grid g-3">
+                        <a class="btn btn-transparent" href="http://localhost/sistema_fmtor/produccion/main/ordenes">
+                            <div class="d-grid g-1">
+                                <i class="material-icons-outlined icon">engineering</i>
+                                <span class="material-icons-outlined icon">engineering</span>
+                                <p>ORDENES DE PRODUCCIÓN</p>
+                            </div>
+                        </a>
+                        <a class="btn btn-transparent" href="http://localhost/sistema_fmtor/produccion/main/control">
+                            <div class="d-grid g-1">
+                                <i class="material-icons-outlined icon">supervisor_account</i>
+                                <span class="material-icons-outlined icon">supervisor_account</span>
+                                <p>REGISTRO DIARIO DE PRODUCCIÓN</p>
+                            </div>
+                        </a>
+                        <a class="btn btn-transparent" href="http://localhost/sistema_fmtor/produccion/main/maquinas">
+                            <div class="d-grid g-1">
+                                <i class="material-icons-outlined icon">precision_manufacturing</i>
+                                <span class="material-icons-outlined icon">precision_manufacturing</span>
+                                <p>MÁQUINAS DE PRODUCCIÓN</p>
+                            </div>
+                        </a>
+                        <a class="btn btn-transparent" href="http://localhost/sistema_fmtor/produccion/main/estados">
+                            <div class="d-grid g-1">
+                                <i class="material-icons-outlined icon">new_releases</i>
+                                <span class="material-icons-outlined icon">new_releases</span>
+                                <p>ESTADO DE LAS ORDENES DE PRODUCCIÓN</p>
+                            </div>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
